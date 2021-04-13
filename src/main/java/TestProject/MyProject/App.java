567140c52367b0ccee7d8b74cfd68f19364b3195
Deepthi <pujitsai.k@gmail.com>
2021-04-13 15:05:03 +1000

    My first commit

---
+++ src/main/java/TestProject/MyProject/App.java
@@ -0,0 +1,13 @@
+package TestProject.MyProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/myProject/Test1.java
@@ -0,0 +1,12 @@
+package myProject;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+    @Test
+	public void method1()
+	{
+		System.out.println("Hi");
+	}
+
+}
